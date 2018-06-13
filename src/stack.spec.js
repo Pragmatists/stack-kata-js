416464f4033c599b48a130c7be68ea737cdb89b3
michal.lipski <michal.lipski@newvoicemedia.com>
2018-06-13 13:37:05 +0200

    red

--- src/stack.spec.js
+++ src/stack.spec.js
@@ -6,11 +6,21 @@ describe('Stack', () => {
             return 0
         }
 
+        push(){
+            
+        }
+
     }
 
     it('should be empty when created', () => {
         const stack = new Stack()
         expect(stack.getSize()).toEqual(0)
+    });
+
+    it('size after push is one', () => {
+        const stack = new Stack();
+        stack.push("elem")
+        expect(stack.getSize()).toEqual(1)
     });
 
 
